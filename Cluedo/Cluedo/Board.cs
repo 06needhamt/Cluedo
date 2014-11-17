170e819be0a5b8c39bf5a0c0cd28b35e4bfbda15
06needhamt <06needhamt@gmail.com>
2014-11-17 17:55:28 +0000

    Cluedo For Programming

---
+++ Cluedo/Cluedo/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cluedo
+{
+    class Board
+    {
+        private LinkedList<Square> board = new LinkedList<Square>();
+
+        public Board()
+        {
+            for (int i = 0; i <= 36; i++)
+            {
+                board.AddLast(new Square());
+            }
+        }
+    }
+}
